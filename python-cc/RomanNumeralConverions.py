39265fd6ad196aac9eec0634a59850495bc1c284
GavaTech <james_adamsp@yahoo.com>
2022-02-08 20:52:05 -0600

    add sqllite python

---
+++ python-cc/RomanNumeralConverions.py
@@ -0,0 +1,86 @@
+print("### This program converts decimal numbers to Roman Numberals ###")
+print("To exit the program, please type \"exit\"")
+var = int(input ("PLease enter a number between 1 and 3999, inclusivlely: "))
+
+
+
+
+
+
+def ShowOutput(inputNumber,output):
+    print("Input       Output\n-----       ------")
+    print("{}          {}         ".format(inputNumber,output))
+
+
+
+thisList = [1,5,10,50,100,500,1000]
+th
+
+
+
+
+
+
+if var < 1 or var > 3999:
+    print("val is out of ranage, only can be 1 -3999 or valid numbers.")
+    quit()
+
+
+t = 1
+result = ""
+numeral = ""
+while t != 0:
+    y = 0
+    for x in thisList:
+        if x <= var:
+            y = x
+
+    var = var - y
+    result = result+numeral
+    t = var 
+
+
+
+    
+
+
+
+
+
+  
+
+
+
+
+
+    
+    
+ 
+
+print(result)
+
+    
+
+
+
+
+
+ 
+
+
+
+    
+  
+
+
+
+
+
+            
+
+      
+
+
+
+
+
